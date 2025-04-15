21e0ea4a556abcc27b55ca0a459130e2d1909dc9
Theo <marcinlowercase@gmail.com>
2025-04-15 10:07:16 -0600

    one

--- ui/i/function/boot.js
+++ ui/i/function/boot.js
@@ -3,6 +3,7 @@ const boot = () => {
   tom.style.opacity = "1";
   name.style.opacity = "0";
   setTimeout(() => {
+    name.style.opacity = "1";
     if (!lock && !hoverring) tom.style.opacity = "0";
     booting = false;
   }, 500);

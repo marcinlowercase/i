573d78a726acd3d264f795ff001067aa83ee8dd9
marcinlowercase <marcinlowercase@gmail.com>
2025-05-12 09:02:23 -0600

    one

--- ui/i/function/email.js
+++ ui/i/function/email.js
@@ -1,15 +1,33 @@
-const emailDiv = document.getElementById("email");
+// const email_div = document.getElementById("email");
 
-emailDiv.addEventListener("click", () => {
-  const email = emailDiv.textContent;
+// email_div.addEventListener("click", () => {
+//   const email = email_div.textContent;
 
-  // Use Clipboard API to copy the email
+//   // Use Clipboard API to copy the email
+//   navigator.clipboard
+//     .writeText(email)
+//     .then(() => {
+//       alert("marc in your clipboard");
+//     })
+//     .catch((err) => {
+//       console.error("fail to access clipboard", err);
+//     });
+// });
+
+const email_element = document.getElementById("email");
+
+email_element.addEventListener("click", () => {
+  const email = email_element.textContent;
+
   navigator.clipboard
     .writeText(email)
     .then(() => {
-      alert("Email copied to clipboard!");
+      prompt(
+        "My email copied to clipboard. You can paste it with Ctrl+V:",
+        email,
+      );
     })
     .catch((err) => {
-      console.error("Failed to copy email: ", err);
+      console.error("Failed to access clipboard", err);
     });
 });

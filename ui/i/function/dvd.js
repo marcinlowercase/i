47a4808027e62c90fb623fbf59382d27a80358f3
Tom <marcinlowercase@gmail.com>
2025-10-13 17:57:23 -0600

    one

--- ui/i/function/dvd.js
+++ ui/i/function/dvd.js
@@ -1,7 +1,7 @@
 // dvd.js
 const start_dvd_bounce = (id, originalX, originalY, zoom, speed) => {
-  if (window.matchMedia("(max-width: 1000px)").matches) {
-    console.log("max-width< 1000px");
+  if (window.matchMedia("(max-width: 820px)").matches) {
+    console.log("max-width< 820px");
     return;
   }
 

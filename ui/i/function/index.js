24d799f6a4b817b2a617f1fce50d6fc87d43aa09
Tom <marcinlowercase@gmail.com>
2025-10-13 20:02:50 -0600

    one

--- ui/i/function/index.js
+++ ui/i/function/index.js
@@ -209,8 +209,6 @@ const on_touch_device = () => {
 };
 
 const on_device_has_mouse = () => {
-  console.log(window.APP_CONFIG);
-  if (window.APP_CONFIG) console.log("APP_CONFIG exists");
   if (window.APP_CONFIG && window.APP_CONFIG.hasSimulatedCursor) {
     // If the server told us the cursor is simulated, we lie and say we have a mouse.
     console.log("Simulated cursor detected! Faking mouse presence.");
